fc013f9d8ec86c51952f8a95165be0ebc2e53c15
Chuck Sylvester <chuck.sylvester@outlook.com>
2025-01-13 17:26:16 -0500

    Coding from cpsmba23

---
+++ _docs/design-spec/db_schema/cdwwork/create_db_cdwwork.sql
@@ -0,0 +1,12 @@
+/*
+ * create_db_cdwwork.sql
+ *
+ * This script creates the database: cdwwork
+ * It is based on the CDW CDWWork database
+ * 
+ */
+
+-- Create database: cdwwork
+
+DROP DATABASE IF EXISTS cdwwork;
+CREATE DATABASE cdwwork;
